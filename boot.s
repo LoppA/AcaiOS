77a805044fdaac92417ccd10218327dfe1c6b6bd
LoppA <Lopacheco@outlook.com>
2016-09-26 14:11:21 -0300

    boot

--- boot.s
+++ boot.s
@@ -1,10 +1,27 @@
+.section	.data
+op:	.ascii	"1"
+
 .section	.text
 .globl		_start
-.code16
+#.code16
 
-_start:
-		nop
-loop:		jmp loop
+_start:	
+loop:
+	# Ler
+	movl	$3,	%eax
+	movl	$1,	%ebx
+	movl	$op,	%ecx
+	movl	$1,	%edx
+	int	$0x80
+	
+	# Imprimir
+	movl	$4,	%eax
+	movl	$2,	%ebx
+	movl	$op,	%ecx
+	movl	$1,	%edx
+	int	$0x80
 
-. = _start + 510
-.byte		0x55, 0xAA
+	jmp 	loop
+
+#. = _start + 510
+#.byte		0x55, 0xAA
