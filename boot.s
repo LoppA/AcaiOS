1e74aa9b84134d6427faeaa0746119d8e7870fe5
LoppA <Lopacheco@outlook.com>
2016-09-27 10:54:56 -0300

    Funcao Clear Pronta

--- boot.s
+++ boot.s
@@ -3,28 +3,50 @@
 
 _start:
 .code16
-	xorw	%ax, %ax
-	movw	%ax, %ds
-	movw	%ax, %ss
-	movw	%ax, %fs
+	xorw	%ax, 	%ax
+	movw	%ax, 	%ds
+	movw	%ax, 	%ss
+	movw	%ax, 	%fs
 	jmp 	start
 
 start:
-loop:
-	#Ler, fica no al	
-	xorw	%ax, %ax
+	# Ler, fica no al	
 	int	$0x16
 	
-	movb	$0x1E, %ah
-	movl	$0xB8000, %ecx
-	movw	%ax, (%ecx)
+	# Imprimir na primeira posicao da tela
+	movb	$0x1E, 		%ah
+	movl	$0xB8000,	%ecx
+	movw	%ax, 		(%ecx)
 	
- jmp	loop
+jmp	_start
 
- clear:
+clear:
+	movb	$2,		%ah
+	movb	$0,		%bh
+	xorw	%dx,		%dx
+	int	$0x10
 
+loop_clear:
 
- jmp	loop
+	movb	$0x0E,		%ah
+	movb	$' ',		%al
+	xorw	%bx,		%bx
+	int	$0x10
+
+	addw	$1,		%dx
+
+	cmp	$2000,		%dx
+
+	jl	loop_clear
+
+	movb	$2,		%ah
+	movb	$0,		%bh
+	xorw	%dx,		%dx
+	int	$0x10
+
+jmp	loop
+
+loop:	jmp	loop
 
 . = _start + 510
 .byte	0x55, 0xAA
