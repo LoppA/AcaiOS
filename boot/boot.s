24060c915cc4290066792dfce025760aac5af43f
LoppA <Lopacheco@outlook.com>
2016-10-11 19:35:21 -0300

    Jmp mudaram para calls, reboot quase terminado, falta desolacar curso. Criada funcao mover_cursor

--- boot/boot.s
+++ boot/boot.s
@@ -4,13 +4,50 @@
 _start:
 .code16
 	# Move cursor para a linha 12, coluna 0
-	movb	$2,	%ah
-	movb	$0,	%bh
 	movb	$0x0C,	%dh
 	movb	$0,	%dl
-	int	$0x10
+	call	move_cursor
 
 	jmp 	start
+
+# Move cursor para as posicoes marcadas em dh, dl
+move_cursor:
+	movb	$2,	%ah
+	movb	$0,	%bh
+	int	$0x10
+
+	ret
+
+clear:
+	# Move cursor para inicio
+	xorw	%dx,		%dx
+	call	move_cursor
+
+loop_clear:
+
+	# Coloca ' ' na tela e incrementa cursor
+	movb	$0x0E,		%ah
+	movb	$' ',		%al
+	xorw	%bx,		%bx
+	int	$0x10
+
+	addw	$1,		%dx
+
+	cmp	$2000,		%dx
+
+	jl	loop_clear
+
+	call	move_cursor
+
+	# Move cursor para inicio
+	xorw	%dx,		%dx
+	call	move_cursor
+
+	ret
+
+reboot:
+	int	$0x19
+	ret
 
 start:
 	# Resetar registradores
@@ -30,39 +67,26 @@ start:
 
 	# if (al == '1') clear()
 	cmp	$'1',		%al
-	je	clear
+	je	_clear
+
+
+	# if (al == 4') clear()
+#	cmp	$'4',		%al
+#	je	_reboot
 
 	
 jmp	start
 
-clear:
-	# Move cursor para inicio
-	movb	$2,		%ah
-	movb	$0,		%bh
-	xorw	%dx,		%dx
-	int	$0x10
+_clear:
+	call	clear
+	jmp	start
 
-loop_clear:
+_reboot:
+	call	clear
+	call 	reboot
+	jmp	start
 
-	# Coloca ' ' na tela e incrementa cursor
-	movb	$0x0E,		%ah
-	movb	$' ',		%al
-	xorw	%bx,		%bx
-	int	$0x10
-
-	addw	$1,		%dx
-
-	cmp	$2000,		%dx
-
-	jl	loop_clear
-
-	# Move cursor para inicio
-	movb	$2,		%ah
-	movb	$0,		%bh
-	xorw	%dx,		%dx
-	int	$0x10
-
-jmp	start
+jmp	_start
 
 . = _start + 510
 .byte	0x55, 0xAA
