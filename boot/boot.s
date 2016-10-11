ca633e826b59f975f592f803bd433d5e023ed8ab
LoppA <Lopacheco@outlook.com>
2016-10-11 16:46:59 -0300

    Boot 2

--- boot/boot.s
+++ boot/boot.s
@@ -3,15 +3,34 @@
 
 _start:
 .code16
+	# Move cursor para a linha 12, coluna 0
+	movb	$2,	%ah
+	movb	$0,	%bh
+	movb	$0x0C,	%dh
+	movb	$0,	%dl
+	int	$0x10
+
 	# Resetar registradores
 	xorw	%ax, 	%ax
 	movw	%ax, 	%ds
 	movw	%ax, 	%ss
 	movw	%ax, 	%fs
+
 	jmp 	start
 
 start:
-	nop
+	# Ler, fica no al	
+	int	$0x16
+
+	movb	$0x0A,		%ah
+	movb	$0,		%bh
+	movb	$0XAA,		%bl
+	movw	$1,		%cx
+	int	$0x10
+
+	
+jmp	_start
+
 loop:	jmp	loop
 
 . = _start + 510
