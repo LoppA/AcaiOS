edb9da7edf466d668e6f9cf38a2f8d8803f88eea
Motokubo <jorgehideki10@hotmail.com>
2016-10-11 22:14:46 -0300

    funcao dos 3 dispositivos

--- boot/boot.s
+++ boot/boot.s
@@ -74,6 +74,90 @@ reboot:
 	int	$0x19
 	ret
 
+#mudei aqui
+#imprime dispositivos conectados
+conectados:
+	#chama a syscall que coloca as informacoes no ax
+	int $0x11
+	
+	#imprimo a string e verifica se tem o dispositivo conectado
+	movl	$str_game_adapter,	%ecx
+	call	print_str
+	
+	movw	%ax,	%bx	
+	movw	$0b0010000000000000,		%cx
+	andw	%cx,	%bx
+	cmp		%cx,	%bx
+	je		game_S
+	jmp		game_N
+	
+	#caso tenha ele imprime S, caso n tenha ele imprime N
+	game_S:
+		movb	$'S',	%al
+		jmp game_print
+		
+	game_N:
+		movb	$'N',	%al
+	
+	game_print:
+		call	putchar
+		
+	#funcao que move o cursor para a proxima linha
+	movb	$0x01,	%dh
+	movb	$0x00,	%dl
+	call	move_cursor
+	
+	
+	
+	movl	$str_internal_modem,	%ecx
+	call	print_str
+	movw	%ax,	%bx	
+	movw	$0b0100000000000000,		%cx
+	andw	%cx,	%bx
+	cmp		%cx,	%bx
+	je		modem_S
+	jmp		modem_N
+	
+	modem_S:
+		movb	$'S',	%al
+		jmp modem_print
+		
+	modem_N:
+		movb	$'N',	%al
+	
+	modem_print:
+		call	putchar
+		
+		
+	movb	$0x02,	%dh
+	movb	$0x00,	%dl
+	call	move_cursor
+	
+	movl	$str_printer_ports,	%ecx
+	call	print_str
+	movw	%ax,	%bx	
+	movw	$0b1000000000000000,		%cx
+	andw	%cx,	%bx
+	cmp		%cx,	%bx
+	je		printer_S
+	jmp		printer_N
+	
+	printer_S:
+		movb	$'S',	%al
+		jmp printer_print
+		
+	printer_N:
+		movb	$'N',	%al
+	
+	printer_print:
+		call	putchar
+	
+	movb	$0x04,	%dh
+	movb	$0x00,	%dl
+	call	move_cursor
+	
+	ret
+	
 ram:
 	int	$0x12
 	movb	$'0',	%al
@@ -108,6 +192,10 @@ start:
 	cmp	$'1',		%al
 	je	_clear
 
+	#mudei aqui
+	# if (al == '3') conectados()
+	cmp	$'3',		%al
+	je	_conectados
 
 	# if (al == 4') reboot()
 #	cmp	$'4',		%al
@@ -130,6 +218,12 @@ _reboot:
 	call 	reboot
 	jmp	start
 
+#mudei aqui	
+_conectados:
+	call clear
+	call conectados
+	jmp start
+
 _ram:
 	call	clear
 	call 	ram
@@ -137,7 +231,12 @@ _ram:
 
 jmp	_start
 
-str_ram:	.ascii	" KB of memory avaliable on RAM"
+str_ram:	.ascii	" KB of memory avaliable on RAM\0"
+
+#mudei aqui
+str_game_adapter:	.ascii	" Game adapter: \0"
+str_internal_modem:	.ascii	" Internal modem: \0"
+str_printer_ports:	.ascii	" Printer ports: \0"
 
 . = _start + 510
 .byte	0x55, 0xAA
