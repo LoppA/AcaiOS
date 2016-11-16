9e068cb07397ec233b30d5024867d3404787ddfc
motokubo <jorgehideki10@hotmail.com>
2016-11-16 00:20:45 -0200

    add string.h

---
+++ kernel/include/string.h
@@ -0,0 +1,12 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+int strlen(const char *);
+
+char *strcpy(char *, const char *);
+
+void *memcpy(void *, const void *, int);
+
+int strcmp(const char *, const char *);
+
+#endif
