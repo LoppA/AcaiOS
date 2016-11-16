d259ad6a8bb4c4836a7b2a894150925060604797
LoppA <Lopacheco@outlook.com>
2016-11-16 20:12:52 -0200

    vga corrigido

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -3,7 +3,9 @@
 void kernel_main () {
 	int foo = 10;
 
-	clear_screen();
+//	clear_screen();
+
+//	putchar ('Q', WHITE, BLACK);
 
 	printf ("Inteiro: \t%d\n", foo);
 	printf ("Binario: \t%b\n", foo);
--- kernel/src/vga.c
+++ kernel/src/vga.c
@@ -15,38 +15,34 @@ void set_character_color (enum color letterColor, enum color bgColor)
 
 void clear_screen() {
     uint32_t i;
-    for (i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
-        pos = i;
-        print_character(0, BLACK, BLACK);
-    }
+	set_character_color (BLACK, BLACK);
+    for (i = 0; i < 2*SCREEN_HEIGHT * SCREEN_WIDTH; i+=2) {
+		*(screen_start+i) = ' ';
+		*(screen_start+i + 1) = characterColor;
+	}
 	pos = 0;
 }
 
 void print_character (char letter, enum color letterColor, enum color bgColor) {
     //cada impressao exige dois bytes, um indicando a cor e outro o caracter a ser impresso
-	int f = 1;
-
-    set_character_color(letterColor, bgColor);
-    charac = characterColor;
-    charac = charac << 8; //posiciona a cor no primeiro byte
-    charac = charac | letter;
 
 	if (letter == '\n') {
 		pos += 2*(SCREEN_WIDTH - ((pos/2)%SCREEN_WIDTH));
-		f = 0;
 	} else if (letter == '\t') {
-		pos += 2*(8 - ((pos/2)%8));
-		f = 0;
-	}
-   
-	if (f) {
-	    *(screen_start+pos) = charac;
+		set_character_color (BLACK, BLACK);
+		for ( ; pos < (pos/2)%8; pos+=2) {
+			*(screen_start + pos) = ' ';
+			*(screen_start + pos + 1) = characterColor;
+		}
+	} else { 
+    	set_character_color(letterColor, bgColor);
+	    *(screen_start+pos) = letter;
+		*(screen_start+pos+1) = characterColor;
 	    pos+=2;//posicao se move de 2 em 2 bytes
 	}
 
     if (pos >= SCREEN_HEIGHT * SCREEN_WIDTH) {
         //limpar tela e voltar pos para 0
         clear_screen();
-        pos = 0;
     }
 }
