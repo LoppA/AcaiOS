d29c799d74abab8bd5911cad662f80ef5a2b7e1a
LoppA <Lopacheco@outlook.com>
2016-11-16 16:50:31 -0200

    va_list corrigida

--- kernel/include/stdarg.h
+++ kernel/include/stdarg.h
@@ -1,7 +1,7 @@
 #ifndef _STDARG_H_
 #define _STDARG_H_
 
-#define va_list
+typedef __builtin_va_list va_list;
 
 #define va_start(ap, param) __builtin_va_start(ap, param)
 #define va_end(ap)          __builtin_va_end(ap)
